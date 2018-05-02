1aaae80f68c8383f6ce26425b4cdc1ec45ecaae9
Jason Preszler <jpreszler@gmail.com>
2018-05-02 09:25:46 -0600

    started scraping courses

--- scrape/catalog-scrape.R
+++ scrape/catalog-scrape.R
@@ -11,7 +11,7 @@ base_html <- read_html(paste0(base_url,base_url_ext))
 subjectLinks <- html_nodes(base_html, 'a')
 #convert links to text
 subjectText <- html_text(subjectLinks)
-#pick out corresponds to subject list
+#pick out what corresponds to subject list
 #we now have subject codes and names
 subjectText <- subjectText[78:120]
 
@@ -20,5 +20,16 @@ names(subDF) <- c("sub", "subject")
 
 #get url for subject, for each row in subDF
 subDF <-subDF %>% mutate(url= paste0(base_url,base_url_ext,'/',sub,'-',str_replace_all(subject,' ','-')))
-#we can also get the actual links
-lapply(78:120, function(x) subjectLinks[[x]])
+
+get_class_list <- function(sub_url, sub_code){
+  #get list of links on subject page
+  class_links <- html_nodes(read_html(sub_url), 'a')
+  #turn links to text
+  class_list <- html_text(class_links)
+  #only keep links for classes, each subject has 
+  #classes starting in a different position
+  class_list <- class_list[str_detect(class_list, paste0(sub_code,'-'))]
+  #now build class dataframe with sub,number,url - we'll get the
+  #name and other details on the next scrape
+  
+}
