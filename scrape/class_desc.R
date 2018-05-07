5070dcb1fb212e14b32c81fb30dee17473509257
Jason Preszler <jpreszler@gmail.com>
2018-05-06 18:13:33 -0600

    prereqs, desc, and credits scraped - testing needed

--- scrape/class_desc.R
+++ scrape/class_desc.R
@@ -13,7 +13,7 @@ get_descriptions <- function(class_url){
   #replace newlines with spaces
   course_desc_text <- str_replace_all(course_desc_text, '\n', " ")
   #split out prerequisite descriptions
-  course_pre <- str_split(course_desc_text, "Prerequisite.:", n=2, simplify = TRUE)[,2]
+  course_pre <- str_split(course_desc_text, 'Prerequisite[s]?:', n=2, simplify = TRUE)[,2]
   
   #credits
   course_cred <- html_nodes(course_page, '.credits') %>% 
