e9a48b1efaa22c28d6ec05d710d58ee02c63b3f0
Jason Preszler <jpreszler@gmail.com>
2018-06-05 09:22:36 -0600

    added prereq from basic parsing

--- clean/prereq-parse.R
+++ clean/prereq-parse.R
@@ -5,8 +5,25 @@ prereq_parse <- function(classDF){
   #pattern for course specifier MAT-175 or PHY-271L
   course_pattern <- "[:upper:]{3}-[:digit:]{3}[LT]?"
   perm_pattern <- "(Instructor permission)|Permission"
-  standing_pattern <- "(Junior)|(Senior)|(Junior or Senior) Standing"
-  placement_pattern <- "Math (P|p)lacement"
+  standing_pattern <- "(Junior)|(Senior)|(Junior or Senior) (S|s)tanding"
+  math_placement_pattern <- "Math (P|p)lacement"
+  placement_pattern <- "(P|p)lacement"
+  
+  classDF <- select(classDF, sub, number, prerequisites, corequisites)
+
+  classBlank <- classDF[classDF$prerequisites == "",]
+  class0 <- classDF[str_count(classDF$prerequisites, course_pattern)==0,] 
+  
+  classPR <- anti_join(anti_join(classDF, classBlank),class0)
+  
+  #build matrix, each row is a course in classPR each column is course in prereq string
+  prm <- str_extract_all(classPR$prerequisites, course_pattern, simplify = TRUE)
+  prdf <- map_dfr(1:dim(prm)[2], function(x) prdf <- data.frame(id1=paste0(classPR$sub, classPR$number), id2=prm[,x]))
+  prdf <- prdf[prdf$id2!="",]
+  prdf$id2 <- str_remove(prdf$id2,"-")
+  prdf <- arrange(prdf, prdf$id1)
+  
+  return(prdf)
 }
 
 #########################
