7d6c09c74c97b355ba53210622c13291a778675e
Jason Preszler <jpreszler@gmail.com>
2018-07-27 12:22:29 -0600

    old-cat-scrape

--- scrape/class-list-old-cat.R
+++ scrape/class-list-old-cat.R
@@ -6,17 +6,17 @@ library(tidyr)
 
 #url for courses in catalog
 base_url <- "http://collegeofidaho.smartcatalogiq.com"
-base_url_ext <- "/en/current/Undergraduate-Catalog/Courses"
+base_url_ext <- "/2016-2017/Undergraduate-Catalog/Courses"
 
 base_html <- read_html(paste0(base_url,base_url_ext))
 #extract links from base page
 subjectLinks <- html_nodes(base_html, 'a')
 #convert links to text
 subjectText <- html_text(subjectLinks)
-sub_url <- html_attr(subjectLinks, 'href')[79:122]
+sub_url <- html_attr(subjectLinks, 'href')[78:120]
 #pick out what corresponds to subject list
 #we now have subject codes and names
-subjectText <- subjectText[79:122]
+subjectText <- subjectText[78:120]
 
 subDF <- str_split(subjectText,pattern= '-', n=2, simplify=TRUE) %>% as.data.frame()
 names(subDF) <- c("sub", "subject")
